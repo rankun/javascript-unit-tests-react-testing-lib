826064089d265c15475807e7974d4389efd511fc
Roger Tadeu Barreto Santos <roger.tadeu@gmail.com>
2022-09-30 12:21:45 -0300

    feat: app index

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <p>Hello World</p>
+  );
+};
+
+export default App;
